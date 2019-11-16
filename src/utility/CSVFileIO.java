ab9426a3f902a87cd5947376362fe51dc58cf615
Laksh <lakshyajeet22@gmail.com>
2019-11-17 01:05:38 +0800

    Add comments to CSVFileIO

--- src/utility/CSVFileIO.java
+++ src/utility/CSVFileIO.java
@@ -12,17 +12,17 @@ import java.util.List;
 * @author Lakshyajeet Dwivedee 
 */
 public class CSVFileIO {
-    
-    /**
-	 * Store parsed CSV data as a list of CSV rows
+
+	/*
+	 * Store parsed CSV data as a list of CSV rows 
 	 */
     private static ArrayList<CSVRow> CSVInfo = new ArrayList<>();
     
-    /**
-	 * Parses CSV and returns it as a list of list of attributes in String form
+    /*
+     * Parses CSV and returns it as a list of list of attributes in String form
 	 * @param path Path to the CSV file which has to be read
 	 * @return The ArrayList of CSVRow Arraylists
-	 */
+     */
     public static ArrayList<CSVRow> getParsedCSV(String path) {
     	
     	CSVInfo.clear();
@@ -75,11 +75,11 @@ public class CSVFileIO {
         }
         return CSVInfo;
     }
-    
-    /**
-   	 * Takes in a CSVRow and saves it to a CSV file
-   	 * @return The ArrayList of CSVRow Arraylists
-   	 */
+
+    /*
+     * Takes in a CSVRow and saves it to a CSV file
+   	 * @return The ArrayList of CSVRow Arraylists 
+     */
        public static void writeToCSV(String path, CSVRow csvRow) {
     	   FileWriter csvWriter = null;
     	   try {
@@ -98,8 +98,10 @@ public class CSVFileIO {
     	   
        }
        
-       /**
-        * To ReWrite a CSV
+       /*
+        * Rewrites a CSV file
+        * @param path Path of the CSV file
+        * @param table List containing CSVRow objects
         */
        public static void rewriteToCSV(String path, List<CSVRow> table) {
     	   
